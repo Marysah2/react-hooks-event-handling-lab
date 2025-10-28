5a4977a1991a6c18a9235ea02030330c0676b692
Marysah <Marysah2@gmail.com>
2025-10-28 15:49:31 +0300

    Event handling solved

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    return ( <button onBlur={handleBlur} onFocus={handleFocus} >Eyes on me</button>)
+  function handleFocus() {
+    console.log("Good!");
+  }
+
+  function handleBlur() {
+    console.log("Hey! Eyes on me!");
+  }
+}
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
+    function handleInput() {
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
+        <div onChange={handleInput}>
+            <input 
+            name="password"
+            type="password" 
+            placeholder="Enter your Password"            
+            />
+        </div>
     )
 }
 
